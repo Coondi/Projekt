dee7811fe74a792d6fb456dd314af943e43d968b
Coondi <konradbywald@gmail.com>
2016-12-07 16:13:30 +0100

    Piętnasty commit

---
+++ Projekt/Dzwiek.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt
+{
+    class Dzwiek
+    {
+        public string Key { get; set; }
+        public string Value { get;  set; }
+
+        public Dzwiek()
+        {
+
+        }
+
+        public Dzwiek(string Key, string Value)
+        {
+            this.Key = Key;
+            this.Value = Value;
+        }
+    }
+}
