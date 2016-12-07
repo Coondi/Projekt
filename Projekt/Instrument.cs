e54fa3957e901413ec636a51d92ac5141b61aa07
Coondi <konradbywald@gmail.com>
2016-12-07 15:56:20 +0100

    Trzynasty commit

--- Projekt/Instrument.cs
+++ Projekt/Instrument.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace Projekt
 {
@@ -10,6 +13,7 @@ namespace Projekt
     {
         public Dictionary<string, string> dzwieki = new Dictionary<string, string>();
 
+        [System.Xml.Serialization.XmlIgnoreAttribute]
         public string Name
         {
             get
@@ -28,6 +32,17 @@ namespace Projekt
             dzwieki.Add("A", "");
             dzwieki.Add("H", "");
             dzwieki.Add("C2", "");
+        }
+
+        public void Serialization()
+        {
+            XmlSerializer xml = new XmlSerializer(this.GetType());
+
+            TextWriter writer = new StreamWriter(this.GetType().Name + ".xml");
+            xml.Serialize(writer, this);
+            writer.Close();
+            xml = null;
+            writer = null;
         }
     }
 }
